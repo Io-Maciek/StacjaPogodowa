4dd538239e5001e74461efff0267ee78c724780c
Maciej <74189817+Io-Maciek@users.noreply.github.com>
2022-11-20 18:18:51 +0100

    zegar update

--- classess/zegar.py
+++ classess/zegar.py
@@ -10,14 +10,13 @@ class Zegar:
     sekunda = 0
     
     def __init__(self):
-        time_tuple = time.localtime()
+        self.time_tuple = time.localtime()
         
-        self.rok = time_tuple[0]
-        self.godzina = time_tuple[3]
-        self.minuta = time_tuple[4]
-        self.sekunda = time_tuple[5]
+        self.rok = self.time_tuple[0]
+        self.godzina = self.time_tuple[3]
+        self.minuta = self.time_tuple[4]
+        self.sekunda = self.time_tuple[5]
     
     def __str__(self):
         return "{0:02d}:{1:02d}:{2:02d}".format(self.godzina,self.minuta,self.sekunda)
     
-
